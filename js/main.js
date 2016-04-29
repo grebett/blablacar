277feaaa763abc33388532d5820438c7217c6c9b
grebett <gregory.betton@gmail.com>
2016-04-29 12:22:21 +0200

    fix: trying to URL paths

--- js/main.js
+++ js/main.js
@@ -5,32 +5,32 @@
 			next();
 		});
 
-		page('/about', function (ctx) {
+		page('/blablacar/about', function (ctx) {
 			$('.page').css({display:'none'});
 			$('#about').css({display:'block'});
 			console.log(ctx);
 		});
 
-		page('/values', function (ctx) {
+		page('/blablacar/values', function (ctx) {
 			$('.page').css({display:'none'});
 			$('#values').css({display:'block'});
 			console.log(ctx);
 		});
 
-		page('/tech', function (ctx) {
+		page('/blablacar/tech', function (ctx) {
 			$('.page').css({display:'none'});
 			$('#tech').css({display:'block'});
 			console.log(ctx);
 		});
 
-		page('/references', function (ctx) {
+		page('/blablacar/references', function (ctx) {
 			$('.page').css({display:'none'});
 			$('#references').css({display:'block'});
 			console.log(ctx);
 		});
 
 		page('*', () => {
-			page.redirect('/about');
+			page.redirect('/blablacar/about');
 		});
 
 		page({
