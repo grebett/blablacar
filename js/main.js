2781022e750895211f8ec3b4eaaf16d368b38afc
grebett <gregory.betton@gmail.com>
2016-04-29 15:58:32 +0200

    feat: adding responsive design... because why not

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 (function ($) {
 	window.addEventListener('DOMContentLoaded', function (event) {
+		// side nav
+		$(".button-collapse").sideNav();
+
+		// routing
 		page('*', function (ctx, next) {
 			window.scroll(0, 0);
 			next();
@@ -8,21 +12,25 @@
 		page('/blablacar/about', function (ctx) {
 			$('.page').css({display:'none'});
 			$('#about').css({display:'block'});
+			$('.button-collapse').sideNav('hide');
 		});
 
 		page('/blablacar/values', function (ctx) {
 			$('.page').css({display:'none'});
 			$('#values').css({display:'block'});
+			$('.button-collapse').sideNav('hide');
 		});
 
 		page('/blablacar/tech', function (ctx) {
 			$('.page').css({display:'none'});
 			$('#tech').css({display:'block'});
+			$('.button-collapse').sideNav('hide');
 		});
 
 		page('/blablacar/references', function (ctx) {
 			$('.page').css({display:'none'});
 			$('#references').css({display:'block'});
+			$('.button-collapse').sideNav('hide');
 		});
 
 		page('*', function () {
